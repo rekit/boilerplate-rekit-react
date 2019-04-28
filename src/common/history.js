3e70ad4bc1757663878bdbb0a0dded3d6032e36a
Nate Wang <pwang7@ebay.com>
2019-04-28 16:21:20 +0800

    Refine history api.

--- src/common/history.js
+++ src/common/history.js
@@ -1,5 +1,5 @@
-import createHistory from 'history/createBrowserHistory';
+import { createBrowserHistory } from 'history';
 
 // A singleton history object for easy API navigation
-const history = createHistory();
+const history = createBrowserHistory();
 export default history;
